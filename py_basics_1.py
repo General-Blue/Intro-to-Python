52b33d8436f1b80d5b29b4f88f7f620cc58d6f9b
kirk <cameron.ian.kirk@gmail.com>
2021-04-28 19:28:45 -0700

    Cleaned up the repo and created the first program we will work on

---
+++ py_basics_1.py
@@ -0,0 +1,13 @@
+# print hello to the world
+
+# ask the user of your program their name and save it into a variable
+
+# say hello to the user
+
+# import the random library
+
+# use the random library to save a random number into a variable
+
+# print that random number to the user
+
+# multiply the random number by 2 and print the result
